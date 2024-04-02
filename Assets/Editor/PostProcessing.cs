46e4db8e8c5c97be3c3f27661ac9bdc168e673b3
DikobrotherIT <dikobrotherlive@gmail.com>
2024-04-02 16:47:30 +0200

    Init

---
+++ Assets/Editor/PostProcessing.cs
@@ -0,0 +1,29 @@
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using System.IO;
+
+public class IosBuildPostprocessor
+{
+
+    [PostProcessBuild(1)]
+    public static void EditPlist(BuildTarget target, string path)
+    {
+        if (target != BuildTarget.iOS)
+            return;
+
+
+        string plistPath = path + "/Info.plist";
+        PlistDocument plist = new PlistDocument();
+        plist.ReadFromFile(plistPath);
+
+        PlistElementDict rootDict = plist.root;
+
+        // Add ITSAppUsesNonExemptEncryption to Info.plist
+        rootDict.SetString("ITSAppUsesNonExemptEncryption", "false");
+        rootDict.SetString("NSCameraUsageDescription", "true");
+
+
+        File.WriteAllText(plistPath, plist.WriteToString());
+    }
+}
